e18df91655d10fca4106819c8a6c935295c29461
krekus <vkrerer@gmail.com>
2023-10-28 13:47:50 +0300

    edit slider settings

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -2,14 +2,14 @@ import Swiper from '../vendor/swiper.js';
 
 const jurySlider = new Swiper('.jury__slider', {
   slidesPerView: 4,
-  spaceBetween: 40,
+  spaceBetween: 20,
   slidesPerGroup: 1,
   loop: true,
-  loopFillGroupWithBlank: true,
+  // loopFillGroupWithBlank: true,
   direction: 'horizontal',
   breakpoints: {
     0: {
-      slidesPerView: 4,
+      slidesPerView: 1,
       spaceBetween: 0,
     },
     768: {
